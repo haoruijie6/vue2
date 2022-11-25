a98cde4d214b0111984bca0c7bc8e71e0ed9acec
{2327114294@qq.com} <2327114294@qq.com>
2022-11-25 13:45:19 +0800

    elmenet ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-
+import Element from 'element-ui'
 import "./assets/icon/iconfont.css";
 
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
